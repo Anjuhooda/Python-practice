5674aea055430b2dd891814bbdcdf8f795aa1273
Anjuhooda <anjuhudda51@gmail.com>
2025-12-15 18:26:16 +0530

    Python Practice

---
+++ main.py
@@ -0,0 +1,52 @@
+#### list type of data ####
+
+# append #
+
+x=["apple","banana"]
+x.append("mango")
+print(x)
+
+# extend #
+
+x=["apple","banana"]
+y=["mango","orange"]
+x.extend(y)
+print(x)
+
+# pop #
+
+x=["apple","banana","mango"]
+x.pop()
+print(x)
+
+# insert #
+
+x=["apple","banana"]
+x.insert(1,"mango")
+print(x)
+
+# remove #
+
+x=["apple","banana"]
+x.remove("apple")
+print(x)
+
+# sort #
+
+x=[5,4,2,6]
+x.sort()
+print(x)
+
+# clear #
+
+x=["apple","banana","mango"]
+x.clear()
+print(x)
+
+# copy #
+
+x=["orange","pineapple"]
+y=x.copy()
+print(y)
+
+# 
